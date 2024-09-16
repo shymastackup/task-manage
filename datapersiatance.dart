a7156542ffcf6aebc9f3c60ae5ea5eda3d0141d5
shymastackup <shyma.stackup@gmail.com>
2024-09-16 15:07:14 +0530

    version

--- datapersiatance.dart
+++ datapersiatance.dart
@@ -1,9 +1,24 @@
- import 'dart:convert';
- import 'dart:io';
- class Datapersiatance {
+import 'dart:convert';
+import 'dart:io';
+
+class Datapersiatance {
   final String filePath;
 
   Datapersiatance(this.filePath);
+
+
+  Future<void> writeJsonFile(Map<String, dynamic> data) async {
+    try {
+      final file = File(filePath);
+
+      final contents = jsonEncode(data);
+
+      await file.writeAsString(contents, flush: true);
+      print("Data saved successfully to $filePath.");
+    } catch (e) {
+      print("Error writing JSON file: $e");
+    }
+  }
 
   Future<Map<String, dynamic>> readJsonFile() async {
     try {
@@ -11,27 +26,32 @@
 
       if (!await file.exists()) {
         print("File not found, creating a new one.");
+        await file.writeAsString('{}');
         return {};
       }
 
       final contents = await file.readAsString();
+
       if (contents.isEmpty) {
         return {};
       }
-      return jsonDecode(contents);
+
+      final jsonData = jsonDecode(contents);
+
+      if (jsonData is List) {
+        print("JSON data is a list. Converting to a map.");
+        return {'projects': jsonData};
+      }
+
+      if (jsonData is Map<String, dynamic>) {
+        return jsonData;
+      }
+
+      print("Unexpected JSON format.");
+      return {};
     } catch (e) {
       print("Error reading JSON file: $e");
       return {};
     }
   }
-
-  Future<void> writeJsonFile(Map<String, dynamic> data) async {
-    try {
-      final file = File(filePath);
-      final contents = jsonEncode(data);
-      await file.writeAsString(contents);
-    } catch (e) {
-      print("Error writing JSON file: $e");
-     }
-   }
- }
+}
